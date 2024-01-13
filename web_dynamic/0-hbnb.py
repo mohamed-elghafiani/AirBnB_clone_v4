06ca99f68ddea8df8485a8953ae5e15833444e8c
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-01-13 09:17:30 -0800

    added uuid to link

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -24,6 +26,7 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+    cache_id = uuid.uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -37,7 +40,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
